2657a2bc6dcd3cc0dc98632d2ca151fe97f138b9
Jamie Hendrickson <jamie.o.hendrickson@gmail.com>
2025-05-21 11:33:54 -0700

    Completes activity, all tests passing

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,9 @@
-export const mystery3 = (string1, string2) => {};
+export const mystery3 = (string1, string2) => {
+  let count = 0;
+  for (let i = 0; i < string1.length; i++) {
+    if (string1[i] !== string2[i]) {
+      count++;
+    }
+  }
+  return count;
+};
